6c9fa6959f7e83ba25aea9bd565d77b0562801ac
vivianjiang <yjiang@medallionananlytics.com>
2014-05-14 17:23:08 -0400

    update the reademail method

--- readEmail.rb
+++ readEmail.rb
@@ -1,9 +1,10 @@
 require 'mail'
 require 'net/imap'
 require 'net/smtp'
-  
-af_order_re = /.*?abercrombie & fitch order #.*?\d+ confirmation.*/
-af_ship_re = /.*abercrombie & fitch order #.\d+ has shipped./
+
+def readEmail  
+   af_order_re = /.*?abercrombie & fitch order #.*?\d+ confirmation.*/
+   af_ship_re = /.*abercrombie & fitch order #.\d+ has shipped./
  
    imap = Net::IMAP.new('imap.gmail.com',993,true)
    imap.login('jiangyy12@gmail.com', 'jane@8612')
@@ -14,15 +15,113 @@ af_ship_re = /.*abercrombie & fitch order #.\d+ has shipped./
    mail = Mail.read_from_string msg
    
    puts "email content:"
-   puts email
+   #puts mail
    m=af_order_re.match(mail.subject)
-    unless m
+    unless m.nil?
       puts "order email"
       puts mail.subject
       puts mail.from
    end
-   puts mail.text_part.body.to_s
-  # puts mail.html_part.body.to_s
+   processEmail( mail.text_part.body.to_s)
+
+   #puts mail.text_part.body.to_s
+   end
+   imap.logout()
+   imap.disconnect() 
 end
-imap.logout()
-imap.disconnect() 
+
+def processEmail(emailContent)
+    orderId_re =/.*?order\s*#:.*?(\d+).*/ 
+    orderDate_re = /.*order\s*date:.*?(\d+\/\d+\/\d+).*/
+    shipDate_re = /.*ship\s*date:.*?(\d+\/\d+\/\d+).*/
+    #subTotal_re = 
+    item_re = /.*?\D(\d{9})\D.*?/
+    price_re = /^\s*(?:price)?\$?(\d+\.\d+).*/
+    start_re = /.*item\s*description.*/
+    end_re = /.*subtotal.*/
+    total_re = /.*discount.*/
+
+    start = false
+    item = ''
+    price = ''
+    items = Hash.new
+    totalStr = ''
+    total = false
+
+     emailContent.each_line do |line|
+         if line.empty?
+            next
+         end
+    
+        if result = orderId_re.match(line.downcase)
+            puts result.captures
+            next
+        end
+
+
+         if result = shipDate_re.match(line.downcase)
+             shipDate = result.captures
+             puts shipDate 
+             next
+          end
+
+         if result = orderDate_re.match(line.downcase)
+             puts result.captures
+             next
+          end
+         
+         if result = start_re.match(line.downcase)
+             start = true
+             next
+         end
+        
+ 
+         if start
+            if result = item_re.match(line.downcase)
+               item = result.captures
+               puts result.captures
+               next
+            end
+
+           if result = price_re.match(line.downcase)
+              price = result.captures
+              items.store(item, price)
+              puts price
+           end 
+         end
+
+         if  total
+             totalStr.concat(line.downcase)
+         end
+
+         if result = end_re.match(line.downcase)
+            start = false
+            totalStr.concat(line.downcase)
+            total = true
+            next 
+         end 
+
+         if result = total_re.match(line.downcase)
+            total = false
+            puts totalStr
+            next
+         end
+
+     end
+      
+    
+end
+
+
+def saveEmail
+
+end
+
+def forwardEmail
+end
+
+def moveEmail
+end
+
+readEmail
+
