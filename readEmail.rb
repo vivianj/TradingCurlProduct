13955f85e92c92895c97e2646ad0eb310a8f3ddb
vivianjiang <yjiang@medallionananlytics.com>
2014-05-22 17:17:39 -0400

    update readEmail program

--- readEmail.rb
+++ readEmail.rb
@@ -4,30 +4,37 @@ require 'net/smtp'
 require 'nokogiri'
 
 def readEmail  
-   af_order_re = /.*?order #.*?\d+ confirmation.*/
-   af_ship_re = /.*?order #.\d+ has shipped./
-   e_receipt_re = /.*?your\s*e-receipt\s*from.*/
+    af_order_re = /.*?order #.*?\d+ confirmation.*/
+    af_ship_re = /.*?order #.\d+ has shipped./
+    e_receipt_re = /.*?your\s*e-receipt\s*from.*/
  
-   imap = Net::IMAP.new('imap.gmail.com',993,true)
-   imap.login('jiangyy12@gmail.com', 'jane@8612')
-   imap.select('AF')
-   mailIds = imap.search(['ALL'])
-   mailIds.each do |id|
-   msg = imap.fetch(id,'RFC822')[0].attr['RFC822']
-   mail = Mail.read_from_string msg
-   
-   puts "email content:"
-   #puts mail
-   m=af_order_re.match(mail.subject)
-    unless m.nil?
-      puts "order email"
-      puts mail.subject
-      puts mail.from
-   end
-   #processEmail( mail.text_part.body.to_s)
+    imap = Net::IMAP.new('imap.gmail.com',993,true)
+    imap.login('jiangyy12@gmail.com', 'jane@8612')
+    imap.select('AF')
+     mailIds = imap.search(['ALL'])
+     mailIds.each do |id|
+        msg = imap.fetch(id,'RFC822')[0].attr['RFC822']
+        mail = Mail.read_from_string msg
+     	puts msg
 
-   #puts mail.html_part.body.decoded
-   puts Nokogiri::HTML(mail.html_part).text
+       	if m=af_order_re.match(mail.subject.downcase)
+           processEmail(mail.decode_body)
+           imap.copy(id, "OrderConfirmation")
+           imap.store(id, "+FLAGS",[:Deleted])  
+        end
+
+        if m=af_ship_re.match(mail.subject.downcase)
+           processEmail(mail.decode_body)
+           imap.copy(id, "ShippingConfirmation")
+           imap.store(id, "+FLAGS",[:Deleted])
+        end
+   
+        if m = e_receipt_re.match(mail.subject.downcase)
+           content = Nokogiri::HTML(mail.decode_body).text
+           processEreceipt(content)
+           imap.copy(id, "ShippingConfirmation")
+           imap.store(id, "+FLAGS",[:Deleted])
+        end
    end
    imap.logout()
    imap.disconnect() 
@@ -83,13 +90,13 @@ def processEmail(emailContent)
             if result = item_re.match(line.downcase)
                item = result.captures
                puts result.captures
-               next
             end
 
            if result = price_re.match(line.downcase)
               price = result.captures
               items.store(item, price)
               puts price
+              next
            end 
          end
 
@@ -121,25 +128,115 @@ def processEmail(emailContent)
 
      end
       
-    
 end
 
-def processEreceipt()
+def processEreceipt(emailContent)
+    storeId_re =/.*?store.*?(\d+).*/
+    trans_re=/.*?trans.*?(\d{4,5}).*/
+    orderDate_re = /.*?date\/time.*?(\d{4}-\d{2}-\d{2}).*?\d{2}:?\d{2}.*/
+    subTotal_re = /.*?subtotal.*?\$?(\d+\.\d{2}).*?total.*?\$?(\d+\.\d{2}).*/
+    tax_re = /.*?subtotal.*?\$(\d+\.\d{2}).*?tax.*?\$(\d+\.\d{2}).*?total.*?\$(\d+\.\d{2}).*/
+    item_re = /.*?(\d{9}).*/
+    price_re = /^\s*\$?(\d+\.\d{2})\s*$/
+    item_price_re = /.*?\$(\d+\.\d{2})\s*(\d{9}).*/     
+    end_re = /.*change.*?due.*/
+    total_re = /.*?subtotal.*?/
+
+    item = ''
+    price = ''
+    items = Hash.new
+    totalStr = ''
+    total = false
+
+     emailContent.each_line do |line|
+         line=line.gsub('\*','')
+
+         if line.empty?
+            next
+         end
+
+
+         if  total
+             totalStr.concat(line.downcase)
+         end
+
+         if result = total_re.match(line.downcase)
+            totalStr.concat(line.downcase)
+            total = true
+            next
+         end
+
+         if result = end_re.match(line.downcase)
+            total = false
+            totalStr = totalStr.split.join(' ')
+            next
+         end
+
+        if result = subTotal_re.match(totalStr)
+           subtotal,total = result.captures
+           puts "total and subtotal"
+           puts subtotal
+           puts total
+           return
+        end
+
+        if result = tax_re.match(totalStr)
+           subtotal,tax,total = result.captures
+           puts subtotal
+           puts tax
+           puts total
+           return
+        end
+
+        if result = storeId_re.match(line.downcase)
+            puts "storeID"
+            puts result.captures
+            next
+        end
+
+        if result = trans_re.match(line.downcase)
+           puts "transId"
+           trans = result.captures
+           puts trans
+        end
+
+        if result = orderDate_re.match(line.downcase)
+           puts "Date:"
+           date=result.captures
+           puts date
+           next
+        end
+
+        if result = item_re.match(line.downcase)
+           item = result.captures
+           puts "item"
+           puts result.captures
+           next
+         end
+
+        if result = price_re.match(line.downcase)
+            price = result.captures
+            items.store(item, price)
+            puts "price"
+            puts price
+            next
+        end
+
+
+         if result = item_price_re.match(line.downcase)
+            price,item0 = result.captures
+            items.store(item,price)
+            puts "line matchs item price"
+            puts item
+            puts price
+            item = item0
+            next
+         end
+
+     end
+
 end
 
-def processHco()
-end
-
-
-def saveEmail
-
-end
-
-def forwardEmail
-end
-
-def moveEmail
-end
 
 readEmail
 
