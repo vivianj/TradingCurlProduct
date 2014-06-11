7f25dc0513f400e8de8b52bc1770c43dea6d99d5
vivianjiang <yjiang@medallionananlytics.com>
2014-06-11 17:09:40 -0400

    update email

--- readEmail.rb
+++ readEmail.rb
@@ -1,62 +1,143 @@
+require 'rubygems'
 require 'mail'
 require 'net/imap'
 require 'net/smtp'
 require 'nokogiri'
-require 'RestClient'
+require 'rest_client'
 require 'json'
+require 'date'
 
 def readEmail  
-    af_order_re = /.*?order #.*?\d+ confirmation.*/
-    af_ship_re = /.*?order #.\d+ has shipped./
+    af_order_re = /.*?order #\d+ confirmation.*/
+    af_ship_re = /.*?order #\d+ has shipped.*/
     e_receipt_re = /.*?your\s*e-receipt\s*from.*/
  
     imap = Net::IMAP.new('imap.gmail.com',993,true)
-    imap.login('jiangyy12@gmail.com', 'jane@8612')
-    imap.select('AF')
+    imap.login('kangyihong001@gmail.com', '831218xx')
+    imap.select('test2')
     destFolder = 'other'
     mailIds = imap.search(['ALL'])
     mailIds.each do |id|
         msg = imap.fetch(id,'RFC822')[0].attr['RFC822']
         mail = Mail.read_from_string msg
         data = Hash.new
+        body_type = mail.content_type
+        
+        subject = mail.subject.downcase
+        data['brand'] = getBrandName(subject)
+ 
+        content = getEmailContent(mail)
 
-       	if m=af_order_re.match(mail.subject.downcase)
-           data = processEmail(mail.decode_body)
+       	if m=af_order_re.match(subject)
+           data = processEmail(content)
            destFolder = "OrderConfirmation"
-           data['brand'] = 'AF'
            data['order_status'] = "ordered"
         end
 
-        if m=af_ship_re.match(mail.subject.downcase)
-           data = processEmail(mail.decode_body)
+        if m=af_ship_re.match(subject)
+           data = processEmail(content)
            data['order_status'] = "shipped"
+           data['tracking_no'] = '1234567'
            destFolder = "ShippingConfirmation"
         end
    
-        if m = e_receipt_re.match(mail.subject.downcase)
-           content = Nokogiri::HTML(mail.decode_body).text
+        if m = e_receipt_re.match(subject)
            data = processEreceipt(content)
            data['order_status']="shipped"
-           destFolder = "ShippingConfirmation"
-        end
-          data['email'] = mail.envelope_from
+           data['tracking_no'] = ''
+           destFolder = "test"
+           data['order_no'] = getOrderId(content)  
            
-          print data.to_json
-          response = RestCient.post('http://localhost:3000/api/v1/orders/new_email', data.to_json, :content_type => 'application/json')
-          print response
+           if not /.*?\d{5,}.*?/.match(subject)  
+              newMail = Mail.new
+              newMail = mail
+              newMail.subject = mail.subject<<" Order #"<<data['order_no']
+              puts newMail.subject
+              imap.append(destFolder,newMail.to_s)
+              imap.store(id, "+FLAGS",[:Deleted])
+           end
+        end
+          data['email'] = "bbbear444@gmail.com"
+          
+          puts data.to_json 
+          #response = RestClient.post "http://ourtradingplatform:otp$api*secrect@23.239.13.57/api/v1/orders/new_email", data.to_json, :content_type => :json, :accept => :json
+          #puts response.code
+          #puts response.to_str
 
-          imap.copy(id, destFolder)
-          imap.store(id, "+FLAGS",[:Deleted])
-
+          #imap.copy(id, destFolder)
+          #imap.store(id, "+FLAGS",[:Deleted])
    end
    imap.logout()
    imap.disconnect() 
 end
 
+def getBrandName(subject)
+    if subject.include? 'abercrombie' 
+       return 'Abercrombie & Fitch'
+    elsif subject.include? 'hollister'
+       return 'Hollister'
+    else
+      return 'Other'
+    end
+end
+
+def getEmailContent(message)
+    content = ""
+
+    if message.multipart?
+       message.parts.each do |part|
+         if part.content_type.include? 'plain'
+            content+=part.decode_body.force_encoding('UTF-8') 
+         elsif part.content_type.include? 'html'
+            content+=Nokogiri::HTML(part.decode_body).text  
+         elsif part.multipart?
+               content+=getEmailContent(part)    
+        end
+      end
+   elsif message.content_type.include? 'plain' 
+         content+=message.decode_body
+   elsif message.content_type.include? 'html'
+         content+=Nokogiri::HTML(message.decode_body).text
+   end  
+
+  return content 
+end
+
+def getOrderId(emailContent) 
+    storeId_re = /.*?store\s*(\d+).*?/
+    transId_re = /.*?trans\W*?(\d+).*?/
+    dateId_re = /.*?date\/time.*?(\d{4}-\d{2}-\d{2}).*/
+    orderId = ''
+    
+    emailContent.each_line do |line|
+         line = line.gsub("/[*|>]|=09/","").downcase<<' '
+         line = line.strip
+         
+         if line.empty?
+            next
+         end
+        
+         if result = storeId_re.match(line)
+            orderId << result.captures[0]
+            next
+         end
+         
+         if result = transId_re.match(line)
+            orderId << result.captures[0]
+         end
+
+         if result = dateId_re.match(line)
+            orderId << result.captures[0].gsub('-','')
+            return orderId
+         end
+   end 
+end
+
+
 def processEmail(emailContent)
-    orderId_re =/.*?order\s*#:.*?(\d+).*/ 
-    orderDate_re = /.*order\s*date:.*?(\d+\/\d+\/\d+).*/
-    shipDate_re = /.*ship\s*date:.*?(\d+\/\d+\/\d+).*/
+    orderId_re =/.*?order.*?#:\D*(\d+).*?/ 
+    orderDate_re = /.*?order\s*date:.*?(\d{2}\/\d{2}\/\d{4}).*/
+    shipDate_re = /.*?ship\s*date:.*?(\d+\/\d+\/\d+).*/
     subTotal_re = /.*?subtotal.*?\$?(\d+\.\d{2}).*?shipping.*?\$?(\d+\.\d{2}).*?tax.*?\$?(\d+\.\d{2}).*?total.*?\$?(\d+\.\d{2}).*/
     item_re = /.*?\D?(\d{9})\D?.*?/
     price_re = /^\s*(?:price)?\$?(\d+\.\d+).*/
@@ -74,28 +155,32 @@ def processEmail(emailContent)
     dataH = Hash.new
 
      emailContent.each_line do |line|
-         line = line.gsub("/[*|>]|=09/","").downcase + ' '
+         line = line.gsub("/[*|>]|=09/","").downcase<<' '
          line = line.strip
 
          if line.empty?
             next
          end
-    
+   
+         puts line
+ 
          if result = orderId_re.match(line)
-            orderId = result.captures
-             dataH['order_no']=orderId 
+            puts line 
+            orderId = result.captures[0]
+            dataH['order_no']=orderId 
             next
          end
 
          if result = shipDate_re.match(line)
-             shipDate = result.captures
-             dataH['shipping_date'] = shipDate
-             puts shipDate 
+             shipDate = result.captures[0]
+             dataH['shipping_date'] = Date.parse(shipDate).strftime("%Y-%m-%d")
              next
           end
 
          if result = orderDate_re.match(line)
-             dataH['order_date']=result.captures
+             orderDate = result.captures[0]
+             puts orderDate.length
+             dataH['order_date'] = Date.parse(orderDate).strftime("%Y-%m-%d")
              next
           end
          
@@ -107,13 +192,13 @@ def processEmail(emailContent)
  
          if start
             if result = item_re.match(line)
-               item = result.captures
-               item0['code'] = item
+               item = result.captures[0]
+               item0['code'] = item.to_i
             end
 
            if result = price_re.match(line)
-              price = result.captures
-              item0['price'] = price
+              price = result.captures[0]
+              item0['price'] = price.to_f
               items.push(item0)
               next
            end 
@@ -138,14 +223,13 @@ def processEmail(emailContent)
 
         if result = subTotal_re.match(totalStr)
            subtotal,ship,tax,total = result.captures
-           dataH['subtotal'] = subtotal
-           dataH['total'] = total
-           dataH['tax'] = tax
-           dataH['shipping'] = ship
+           dataH['subtotal'] = subtotal.to_f
+           dataH['total'] = total.to_f
+           dataH['tax'] = tax.to_f
+           dataH['shipping'] = ship.to_f
            dataH['items'] = items
            return dataH
          end
-
      end
       
 end
@@ -153,28 +237,36 @@ end
 def processEreceipt(emailContent)
     storeId_re =/.*?store.*?(\d+).*/
     trans_re=/.*?trans.*?(\d{4,5}).*/
+    trans_orderDate_re = /.*?trans.*?(\d+).*?date\/time.*?(\d{4}-\d{2}-\d{2}).*?\d{2}:?\d{2}.*/
     orderDate_re = /.*?date\/time.*?(\d{4}-\d{2}-\d{2}).*?\d{2}:?\d{2}.*/
     subTotal_re = /.*?subtotal.*?\$?(\d+\.\d{2}).*?total.*?\$?(\d+\.\d{2}).*/
     tax_re = /.*?subtotal.*?\$(\d+\.\d{2}).*?tax.*?\$(\d+\.\d{2}).*?total.*?\$(\d+\.\d{2}).*/
     item_re = /.*?(\d{9}).*/
+    item1_re = /.*?(\d{9}).*?\$\d+\s*\.?\d+\s*$/
     price_re = /^\s*\$?(\d+\.\d{2})\s*$/
+    price1_re = /.*?\$\d+\.\d+\s*\$(\d+\.\d+)\s*$/
     item_price_re = /.*?\$(\d+\.\d{2})\s*(\d{9}).*/     
     end_re = /.*change.*?due.*/
     total_re = /.*?subtotal.*?/
 
     item = ''
     price = ''
-    items = Hash.new
+    start = false
+    trans = ''
+    storeId = ''
+    item0 = Hash.new
+    items = Array.new
+    dataH = Hash.new
     totalStr = ''
     total = false
 
      emailContent.each_line do |line|
-         line=line.gsub('\*','').downcase
+         line=line.gsub('\*|\t','').downcase<<' '
+         line = line.strip
 
          if line.empty?
             next
          end
-
 
          if  total
              totalStr.concat(line)
@@ -188,71 +280,95 @@ def processEreceipt(emailContent)
 
          if result = end_re.match(line)
             total = false
+            start = false
             totalStr = totalStr.split.join(' ')
             next
          end
 
         if result = subTotal_re.match(totalStr)
            subtotal,total = result.captures
+           dataH['total'] = total.to_f
+           dataH['subtotal'] = subtotal.to_f
+           dataH['tax'] = 0.00
+           dataH['shipping'] = 0.00
+           dataH['items'] = items
            puts "total and subtotal"
            puts subtotal
            puts total
-           return
+           return dataH
         end
 
         if result = tax_re.match(totalStr)
            subtotal,tax,total = result.captures
-           puts subtotal
-           puts tax
-           puts total
-           return
+           dataH['subtotal'] = subtotal.to_f
+           dataH['tax'] = tax.to_f
+           dataH['total'] = total.to_f
+           dataH['shipping'] = '0.00'
+           dataH['items'] = items
+           return dataH
         end
 
         if result = storeId_re.match(line)
-            puts "storeID"
-            puts result.captures
+            storeId =  result.captures[0]
             next
         end
-
-        if result = trans_re.match(line)
-           puts "transId"
-           trans = result.captures
-           puts trans
+        
+        if result = trans_orderDate_re.match(line)
+           trans,date = result.captures
+           dataH['order_no'] = storeId<<trans<<date.gsub('-','')
+           dataH['order_date'] = date
+           dataH['shipping_date'] = date
+           start = true
+           next
         end
 
         if result = orderDate_re.match(line)
-           puts "Date:"
-           date=result.captures
-           puts date
+           date = result.captures[0]
+           dataH['order_no'] = storeId<<trans<<date.gsub('-','') 
+           dataH['order_date'] = date
+           dataH['shipping_date'] = result.captures[0]
+           start = true
            next
         end
+  
+        if start
+            if result = item_price_re.match(line)
+              price,code = result.captures
+              item0['price'] = price.to_f
+              items.push(item0)
+              item0['code'] = code.to_i
+              next
+            end
 
-        if result = item_re.match(line)
-           item = result.captures
-           puts "item"
-           puts result.captures
-           next
-         end
+           if result = item1_re.match(line)
+              item0['code'] = result.captures[0].to_i
+              next
+           end
 
-        if result = price_re.match(line)
-            price = result.captures
-            items.store(item, price)
-            puts "price"
-            puts price
-            next
+           if result = price1_re.match(line)
+              item0['price'] = result.captures[0].to_f
+              items.push(item0)
+              next
+           end
+
+           if result = item_re.match(line)
+              item0['code'] = result.captures[0].to_i
+              next
+           end
+
+           if result = price_re.match(line)
+              item0['price'] = result.captures[0].to_f
+              items.push(item0)
+              next
+           end
+
         end
 
-
-         if result = item_price_re.match(line)
-            price,item0 = result.captures
-            items.store(item,price)
-            puts "line matchs item price"
-            puts item
-            puts price
-            item = item0
+         if result = trans_re.match(line)
+            trans = result.captures[0]
             next
          end
-
+          
      end
 
 end
